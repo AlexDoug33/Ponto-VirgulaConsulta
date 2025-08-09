0e27bf5eebd15951c8824125eeaa7a3e2512fcbc
Alex  Douglas <alex.almeida@alfaerp.com.br>
2025-08-09 14:45:43 +0200

    Adicionar arquivos de projeto.

---
+++ Ponto&VirgulaConsulta/Data/ApplicationUser.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Identity;
+
+namespace Ponto_VirgulaConsulta.Data
+{
+    // Add profile data for application users by adding properties to the ApplicationUser class
+    public class ApplicationUser : IdentityUser
+    {
+    }
+
+}
